2481a83b73e79482ff071b8f104fb53d6181bbc5
danielefrisanco <daniele.frisanco@gmail.com>
2014-11-10 18:56:57 +0100

    prospettiva y

--- 3dshooter/3dshooter.js
+++ 3dshooter/3dshooter.js
@@ -6,19 +6,19 @@ $(document).ready(function(){
 	var h = $("#canvas").height();
 	
 	//Lets save the cell width in a variable for easy control
-	var cw = 10;
+	var cw = 20;
 	var d;
-	var food;
+	var wall;
 	var score;
 	var offset;
 	//Lets create the snake now
-	var snake_array; //an array of cells to make up the snake
+	var enemy_array; //an array of cells to make up the snake
 	
 	function init()
 	{
 		d = "right"; //default direction
-		create_snake();
-		create_food(); //Now we can see the food particle
+		create_enemy();
+		create_wall();  
 		//finally lets display the score
 		score = 0;
 		
@@ -30,24 +30,36 @@ $(document).ready(function(){
 	}
 	init();
 	
-	function create_snake()
+	function create_enemy()
 	{
-		var length = 5; //Length of the snake
-		snake_array = []; //Empty array to start with
+		var length = 5; // 
+		enemy_array = []; //Empty array to start with
 		for(var i = length-1; i>=0; i--)
 		{
-			//This will create a horizontal snake starting from the top left
-			snake_array.push({x: i, y:0});
+			//This will create a enemies
+			enemy_array.push({x: Math.round(Math.random()*(w-cw)/cw), y: Math.round(Math.random()*(h-cw)/cw)});
 		}
 	}
 	
-	//Lets create the food now
-	function create_food()
+	//Lets create the wall now
+	function create_wall()
 	{
-		food = {
+		/*wall = {
 			x: Math.round(Math.random()*(w-cw)/cw), 
 			y: Math.round(Math.random()*(h-cw)/cw), 
-		};
+		};*/
+		var length = 5; // 
+		wall_array = []; //Empty array to start with
+		for(var i = length-1; i>=0; i--)
+		{
+			 
+			wall_array.push({x: Math.round(Math.random()*(w-cw)/cw), y: Math.round(Math.random()*(h-cw)/cw)});
+		}
+		
+		
+		
+		
+		
 		//This will create a cell with x/y between 0-44
 		//Because there are 45(450/10) positions accross the rows and columns
 	}
@@ -65,8 +77,8 @@ $(document).ready(function(){
 		//The movement code for the snake to come here.
 		//The logic is simple
 		//Pop out the tail cell and place it infront of the head cell
-		var nx = snake_array[0].x;
-		var ny = snake_array[0].y;
+		var nx = enemy_array[0].x;
+		var ny = enemy_array[0].y;
 		//These were the position of the head cell.
 		//We will increment it to get the new head position
 		//Lets add proper direction based movement now
@@ -82,7 +94,7 @@ $(document).ready(function(){
 		//This will restart the game if the snake hits the wall
 		//Lets add the code for body collision
 		//Now if the head of the snake bumps into its body, the game will restart
-		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array))
+		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, enemy_array)&& false)
 		{
 			//restart game
 			init();
@@ -90,57 +102,69 @@ $(document).ready(function(){
 			return;
 		}
 		
-		//Lets write the code to make the snake eat the food
+		//Lets write the code to make the snake eat the wall
 		//The logic is simple
-		//If the new head position matches with that of the food,
+		//If the new head position matches with that of the wall,
 		//Create a new head instead of moving the tail
-		if(nx == food.x && ny == food.y)
+		/*if(nx == wall.x && ny == wall.y)
 		{
 			var tail = {x: nx, y: ny};
 			score++;
-			//Create new food
-			create_food();
+			//Create new wall
+			create_wall();
 		}
 		else
 		{
-			var tail = snake_array.pop(); //pops out the last cell
+			var tail = enemy_array.pop(); //pops out the last cell
 			tail.x = nx; tail.y = ny;
 		}
-		//The snake can now eat the food.
+		*/
+		//The snake can now eat the wall.
 		
-		snake_array.unshift(tail); //puts back the tail as the first cell
+		//enemy_array.unshift(tail); //puts back the tail as the first cell
 		
-		for(var i = 0; i < snake_array.length; i++)
+		for(var i = 0; i < enemy_array.length; i++)
 		{
-			var c = snake_array[i];
+			var c = enemy_array[i];
 			//Lets paint 10px wide cells
-			paint_cell(c.x, c.y);
+			paint_cell(c.x, c.y,"blue");
 		}
 		
-		//Lets paint the food
-		paint_cell(food.x, food.y);
+		//Lets paint the walls
+		
+		for(var i = 0; i < wall_array.length; i++)
+		{
+			var c = wall_array[i];
+			//Lets paint 10px wide cells
+			paint_cell(c.x, c.y,"red");
+		}
+		
+		 
 		//Lets paint the score
 		var score_text = "Score: " + score;
 		ctx.fillText(score_text, 5, h-5);
 		
-		var rel_pos=relative_position(snake_array[0].x,snake_array[0].y,food.x, food.y,0);
+		var rel_pos=relative_position(enemy_array[0].x,enemy_array[0].y,wall.x, wall.y,0);
 			ctx.fillText("X"+rel_pos.x+" Y"+rel_pos.y+"", 20, h-20);
 ctx.fillText("OX"+offset.x+" OY"+offset.y+"", 60, h-60);
  
 	}
 	
 	//Lets first create a generic function to paint cells
-	function paint_cell(x, y)
+	function paint_cell(x, y,color1)
 	{
-
+		
 		x=x-offset.x;
 		y=y-offset.y;
+		
+		// calcolo le dimensioni in base alla prospettiva
+		pcw=Math.round(h/(h/(y)));
 		if (!(x == -1 || x == w/cw || y == -1 || y == h/cw))
 		{
-			ctx.fillStyle = "blue";
-			ctx.fillRect(x*cw, y*cw, cw, cw);
+			ctx.fillStyle = color1;
+			ctx.fillRect(x*cw, y*cw, pcw, pcw);
 			ctx.strokeStyle = "white";
-			ctx.strokeRect(x*cw, y*cw, cw, cw);
+			ctx.strokeRect(x*cw, y*cw, pcw, pcw);
 		}
 	}
 	
@@ -170,11 +194,11 @@ ctx.fillText("OX"+offset.x+" OY"+offset.y+"", 60, h-60);
 	//Lets add the keyboard controls now
 	$(document).keydown(function(e){
 		var key = e.which;
-		//We will add another clause to prevent reverse gear
-		if(key == "37" && d != "right") d = "left";
-		else if(key == "38" && d != "down") d = "up";
-		else if(key == "39" && d != "left") d = "right";
-		else if(key == "40" && d != "up") d = "down";
+		
+		if(key == "37") d = "left";
+		else if(key == "38" ) d = "up";
+		else if(key == "39" ) d = "right";
+		else if(key == "40" ) d = "down";
 		 
 
 
