587529ddb9e2a2054447c520ffd3aadba2cae302
danielefrisanco <daniele.frisanco@gmail.com>
2014-11-20 18:30:03 +0100

    classe Me + spari contati e distanziati

--- 3dshooter/3dshooter.js
+++ 3dshooter/3dshooter.js
@@ -44,12 +44,11 @@ var MS_PER_UPDATE;
 		score = 0;
 		//date mi serve per l'orario
 		date = new Date();
-		previous = Date.now();
-  lag = 0.0;
-  MS_PER_UPDATE=50;//aggiorno ogni 50 millisecondi la logica
+		previous = performance.now();
+		lag = 0.0;
+		MS_PER_UPDATE=50;//aggiorno ogni 50 millisecondi la logica
 		//Lets move the snake now using a timer which will trigger the paint function
 		//every 60ms	
-		
 		
 		
 		create_enemy();
@@ -57,7 +56,9 @@ var MS_PER_UPDATE;
 		if(typeof game_loop != "undefined") clearInterval(game_loop);
 		game_loop = setInterval(paint, 1000/60);
 		offset={x:0,y:0,t:0};
-		me={x:(w-cw)/2,y:(h-cw)/2};// la mia posizione
+		me=new Me( (w-cw)/2, (h-cw)/2,0 ,performance.now());
+		
+	///////	me={x:(w-cw)/2,y:(h-cw)/2};// la mia posizione
 		center={x:(w-cw)/2,y:(h-cw)/2};//centro
 		// 
 		default_dist=distanza(me.x,me.y,0,0);
@@ -91,7 +92,7 @@ var MS_PER_UPDATE;
 			//This will create a enemies
 			//enemy_array.push({x: Math.round(Math.random()*(w-cw)/cw), y: Math.round(Math.random()*(h-cw)/cw)});
 		//	enemy_array.push({x: Math.round(Math.random()*(w-cw)), y: Math.round(Math.random()*(h-cw))});
-			enemy_array.push(new Enemy( Math.round(Math.random()*(w-cw)), Math.round(Math.random()*(h-cw)),Math.random()*360,Date.now()));
+			enemy_array.push(new Enemy( Math.round(Math.random()*(w-cw)), Math.round(Math.random()*(h-cw)),Math.random()*360,performance.now()));
 		}
 	}
 	
@@ -119,40 +120,12 @@ var MS_PER_UPDATE;
 		//Because there are 45(450/10) positions accross the rows and columns
 	}
 	
-	
-	function create_bullet(xpos,ypos,angle,currentTime)
-	{
 	 
-		////// {x:me.x/cw,y:me.y/cw,t:offset.t,time:date.getTime()} 
-		// new Bullet
-		var bullet= {x:xpos,y:ypos,t:angle,time:currentTime,alive:true} ;
-		return bullet;
-		
-		
-		
-		
-		//This will create a cell with x/y between 0-44
-		//Because there are 45(450/10) positions accross the rows and columns
-	}
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	 
 	
 	
 	
@@ -208,6 +181,7 @@ var MS_PER_UPDATE;
 	
 	
 	
+	 
 	
 	
 	
@@ -216,34 +190,7 @@ var MS_PER_UPDATE;
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	
@@ -255,12 +202,13 @@ var MS_PER_UPDATE;
 	//bisogna che quando creo il bullet faccio update bullet
 	//create_bullet(xpos,ypos,angle,currentTime)
 	function Bullet(xpos,ypos,direction,currentTime){
-	this.x=xpos;
-	this.y=ypos;
-	this.t=direction;
-	this.time=currentTime;
-	this.alive=true;
-	this.speed=bullet_speed;
+		this.x=xpos;
+		this.y=ypos;
+		this.t=direction;
+		this.time=currentTime;
+		this.alive=true;
+		this.speed=bullet_speed;
+		this.color="green";
 	}
 	
 	
@@ -270,7 +218,7 @@ var MS_PER_UPDATE;
     {
      //  update_bullet(this);
 	   
-	     var adesso=Date.now();
+	     var adesso=performance.now();
 		 var timeDiff=adesso-this.time+1;
 		 if (timeDiff<1000 && this.alive==true)
 		 {
@@ -289,12 +237,13 @@ var MS_PER_UPDATE;
 	
 	
 	function Enemy(xpos,ypos,direction,currentTime){
-	this.x=xpos;
-	this.y=ypos;
-	this.t=direction;
-	this.time=currentTime;
-	this.alive=true;
-	this.speed=enemy_speed; 
+		this.x=xpos;
+		this.y=ypos;
+		this.t=direction;
+		this.time=currentTime;
+		this.alive=true;
+		this.speed=enemy_speed; 
+		this.color="blue";
 	}
 	
 	
@@ -304,23 +253,73 @@ var MS_PER_UPDATE;
     { 
 	
 	
-	var rd=relative_direction(this.x,this.y,x_target,y_target);
+		var rd=relative_direction(this.x,this.y,x_target,y_target);
 		this.t=   rnd(rd,40) ;
-	     var adesso=Date.now();
-		 var timeDiff=adesso-this.time+1;
-		 this.time=adesso;
-		 if (  this.alive==true)
-		 {
-		  
-		 this.x=this.x-Math.sin(gradiToRadianti(this.t))*timeDiff/1000*	this.speed;
-			 this.y=this.y 	-Math.cos(gradiToRadianti(this.t))*timeDiff/1000*	this.speed ;  
-		 }
-		 else {this.alive=false;}
+		var adesso=performance.now();
+		var timeDiff=adesso-this.time+1;
+		this.time=adesso;
+		if (  this.alive==true)
+		{
+
+			this.x=this.x-Math.sin(gradiToRadianti(this.t))*timeDiff/1000*	this.speed;
+			this.y=this.y 	-Math.cos(gradiToRadianti(this.t))*timeDiff/1000*	this.speed ;  
+		}
+		else {
+			this.alive=false;
+			}
 		 
     }
 	
 	
     Enemy.prototype.draw = function()
+    {
+     //disegno il Enemy
+    }
+	
+	
+	
+	function Me(xpos,ypos,direction,currentTime){
+		this.x=xpos;
+		this.y=ypos;
+		this.t=direction;
+		this.time=currentTime;
+		this.alive=true;
+		this.speed=enemy_speed; 
+		this.color="black";
+		this.ammo=50;
+		this.life =100;
+		this.armor =100;
+		this.last_shot_time=currentTime;
+	}
+	
+	
+	
+	
+    Me.prototype.update = function(x_target,y_target)
+    { 
+	
+	
+		
+		
+		 
+    }
+	
+    Me.prototype.shoot = function( )
+    { 
+		var adesso=performance.now();
+		if (this.last_shot_time+200<=adesso && 	this.ammo>0)
+		{
+			this.last_shot_time =adesso;
+			bullet_array.push(new Bullet(this.x,this.y,offset.t,adesso));
+			this.ammo--;
+
+		}
+		
+		
+		 
+    }
+	
+    Me.prototype.draw = function()
     {
      //disegno il Enemy
     }
@@ -337,7 +336,7 @@ var MS_PER_UPDATE;
 	
 	function update_bullet(bullet)
 	{ 
-		  var adesso=Date.now();
+		  var adesso=performance.now();
 		 var timeDiff=adesso-bullet.time+1;
 		 if (timeDiff<1000 && bullet.alive==true)
 		 {
@@ -351,59 +350,59 @@ var MS_PER_UPDATE;
 	function paint()
 	{
 	
-	 var current = Date.now();
-	 var elapsed = current - previous;
-     previous = current;
-    lag += elapsed;
-  
+		var current = performance.now();
+		var elapsed = current - previous;
+		previous = current;
+		lag += elapsed;
+
 		//To avoid the snake trail we need to paint the BG on every frame
 		//Lets paint the canvas now
 		ctx.fillStyle = "white";
 		ctx.fillRect(0, 0, w, h);
 		ctx.strokeStyle = "black";
 		ctx.strokeRect(0, 0, w, h);
-		
-		 
+
+
 		  //processInput();
-   if(keyPressed["37"]) { offset.t =(offset.t+6)%360;}
-	 if(keyPressed["39"]) { offset.t=(offset.t-6)%360;}
-	 if(keyPressed["38"]) {    //mi devo spostare in su rispetto all'area di gioco ma lo converto nel piano cartesiano
+		if(keyPressed["37"]) { offset.t =(offset.t+6)%360;}
+		if(keyPressed["39"]) { offset.t=(offset.t-6)%360;}
+		if(keyPressed["38"]) {    //mi devo spostare in su rispetto all'area di gioco ma lo converto nel piano cartesiano
 										me.y=me.y- Math.cos(gradiToRadianti(360-offset.t));  
-										 me.x=me.x+Math.sin(gradiToRadianti(360-offset.t));   
-										 offset.y=center.y-me.y;
-										 offset.x=center.x-me.x;
+										me.x=me.x+Math.sin(gradiToRadianti(360-offset.t));   
+										offset.y=center.y-me.y;
+										offset.x=center.x-me.x;
 										  
 										}
-	 if(keyPressed["40"]) {  	   //mi devo spostare in giu rispetto all'area di gioco ma lo converto nel piano cartesiano
-											me.y=me.y+Math.cos(gradiToRadianti(360-offset.t));
-											 me.x=me.x-Math.sin(gradiToRadianti(360-offset.t));   
-												offset.y=center.y-me.y;
-												offset.x=center.x-me.x;
+		if(keyPressed["40"]) {  	   //mi devo spostare in giu rispetto all'area di gioco ma lo converto nel piano cartesiano
+										me.y=me.y+Math.cos(gradiToRadianti(360-offset.t));
+										me.x=me.x-Math.sin(gradiToRadianti(360-offset.t));   
+										offset.y=center.y-me.y;
+										offset.x=center.x-me.x;
 												
 											}
 
   
   
    
-   if(keyPressed["32"]) {//space 
-   //  bullet_array.push({x:me.x/cw,y:me.y/cw,t:offset.t,time:date.getTime()});
-    //bullet_array.push(create_bullet(me.x/cw,me.y/cw,offset.t,current));
- bullet_array.push(new Bullet(me.x,me.y,offset.t,current));
- 
-   //x e y devono essere diversi
-								}
+		 if(keyPressed["32"]) {//space 
+										//  bullet_array.push({x:me.x/cw,y:me.y/cw,t:offset.t,time:date.getTime()});
+										//bullet_array.push(create_bullet(me.x/cw,me.y/cw,offset.t,current));
+										//bullet_array.push(new Bullet(me.x,me.y,offset.t,current));
+										me.shoot();
+										//x e y devono essere diversi
+										}
    
    
    /////////////FINE processInput();
    
     
-     while (lag >= MS_PER_UPDATE)
-  {
-  
-  // eseguo la logica indipendentemente dal rendering
-     update();
-     lag -= MS_PER_UPDATE;
-  }
+		while (lag >= MS_PER_UPDATE)
+		{
+
+			// eseguo la logica indipendentemente dal rendering
+			update();
+			lag -= MS_PER_UPDATE;
+		}
 
  
  
@@ -454,9 +453,9 @@ var MS_PER_UPDATE;
 		 
 		}
 		
-	//	paint me
-		 	 ctx.fillStyle ="black";
-			ctx.fillRect(me.x+offset.x -cw/2,me.y+offset.y -cw/2, cw, cw);
+		//	paint me
+		 ctx.fillStyle ="black";
+		ctx.fillRect(me.x+offset.x -cw/2,me.y+offset.y -cw/2, cw, cw);
 			 
 		//Lets paint the score
 		var score_text = "Score: " + score;
@@ -465,36 +464,19 @@ var MS_PER_UPDATE;
 		 
 		 /// ctx.fillText("  current"+current+"  previous"+previous   +"    lag"+lag+"  elapsed"+elapsed, 25, 25);
        //  ctx.fillText("OX"+offset.x+" OY"+offset.y+" OT"+offset.t+"   meX"+me.x+"   meY"+me.y, 60, h-60);
-         ctx.fillText("mex"+me.x+" mey"+me.y, 60, h-60);
+         ctx.fillText(" LIFE: "+me.life+" ARMOR: "+me.armor+ " AMMO: "+me.ammo , 60, h-60);
 /////////////fine  render();
 		 
 	}
 	//Lets first create a generic function to paint cells
 	function paint_cell(x, y,color1)
 	{
-		
-		
 		 
-		/*// avanti indietro destra sisnistra
-		x=x-offset.x;
-		y=y-offset.y;
-		*/
-		 radianti=gradiToRadianti(offset.t);//converto i gradi in radianti
-	
-		 //traslo da rispetto alla mia posizione a rispetto a (0,0)
-	 	x=x-((me.x) );
-	 	y=y-((me.y )) ;
-		//nel quadrato di gioco 0 gradi stanno al posto dei classici 90 gradi e la y cresce andando in giu 
-		 var xx=(x*Math.cos(radianti)-y*Math.sin(radianti))+offset.x;
-		 y=(x*Math.sin(radianti)+y*Math.cos(radianti)) +offset.y ;
-	
-		 //risistemo rispetto alla mia posizione
-		x=xx;
-	     x=x+((me.x));
-	 	y=y+((me.y ));
-	
-		////alert((distanza(w/2,h,x,y)));
-//Math.round(h/(h/y))*
+		
+		var tpoint=traslate_point(x,y);
+		x=tpoint.x;
+		y=tpoint.y;
+	 
 		// calcolo le dimensioni in base alla prospettiva
 		
 		
@@ -599,6 +581,27 @@ function rnd_snd() {
 	function rnd(mean, stdev) {
     return Math.round(rnd_snd()*stdev+mean);
 	// per una distribuzione normale provare anche ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()) - 3) / 3
+}
+
+
+
+function traslate_point(x,y){
+		//CALCOLO le coordinate del punto dal piano di gioco al piano visuale
+		radianti=gradiToRadianti(offset.t);//converto i gradi in radianti
+	
+		 //traslo da rispetto alla mia posizione a rispetto a (0,0)
+	 	x=x-((me.x) );
+	 	y=y-((me.y )) ;
+		//nel quadrato di gioco 0 gradi stanno al posto dei classici 90 gradi e la y cresce andando in giu 
+		 var xx=(x*Math.cos(radianti)-y*Math.sin(radianti))+offset.x;
+		 y=(x*Math.sin(radianti)+y*Math.cos(radianti)) +offset.y ;
+	
+		 //risistemo rispetto alla mia posizione
+		x=xx;
+	     x=x+((me.x));
+	 	y=y+((me.y ));
+		return{x,y};
+
 }
 	/*TODO
 	-enemies must move
